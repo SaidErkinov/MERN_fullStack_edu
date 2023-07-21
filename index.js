ce5df875bac211e310cec90381b3b24fb75b1215
von <saidkhan.erkinov@gmail.com>
2023-07-21 12:53:06 +0500

    registration/login added

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-import express, { json } from "express";
+import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import key from "../MERN/keys/key.js";
 import mongoose from "mongoose";
 
 import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
 
+import UserModel from "./models/User.js";
+
 mongoose
-  .connect("mongodb+srv://admin:wwwwww@cluster0.3uatsci.mongodb.net/")
+  .connect(
+    "mongodb+srv://admin:wwwwww@cluster0.3uatsci.mongodb.net/blog?retryWrites=true&w=majority"
+  )
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
 
@@ -15,15 +20,91 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+app.post("/auth/login", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
 
-  res.json({
-    success: true,
-  });
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Неверный логин или пароль",
+      });
+    }
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      key,
+      {
+        expiresIn: "30d",
+      }
+    );
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: "Не удалось авторизоваться!",
+    });
+  }
+});
+
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      key,
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось зарегестрироваться!",
+    });
+  }
 });
 
 app.listen(4444, (err) => {
